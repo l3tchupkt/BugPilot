d9334d37e19b1167c152dd420c4c772f9a5b947a
l3tchupkt <letchu.geek@gmail.com>
2025-09-11 15:55:21 +0800

    feat: add storage support for LinearContext
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/kosong/context/linear.py
+++ src/kosong/context/linear.py
@@ -1,3 +1,8 @@
+import asyncio
+import json
+from pathlib import Path
+from typing import IO, Protocol, runtime_checkable
+
 from kosong.base.message import Message
 from kosong.tooling import Toolset
 
@@ -7,10 +12,17 @@ class LinearContext:
     A context that contains a linear history of messages.
     """
 
-    def __init__(self, system_prompt: str, toolset: Toolset):
+    def __init__(
+        self,
+        system_prompt: str,
+        toolset: Toolset,
+        storage: "LinearStorage",
+    ):
         self._system_prompt = system_prompt
         self._toolset = toolset
-        self._history: list[Message] = []
+        # TODO: support forget/compact active messages
+        self._active_messages: list[Message] = []
+        self._storage = storage
 
     @property
     def system_prompt(self) -> str:
@@ -22,7 +34,54 @@ class LinearContext:
 
     @property
     def history(self) -> list[Message]:
-        return self._history
+        return self._active_messages
 
     async def add_message(self, message: Message):
-        self._history.append(message)
+        self._active_messages.append(message)
+        await self._storage.append_message(message)
+
+
+@runtime_checkable
+class LinearStorage(Protocol):
+    async def append_message(self, message: Message) -> None: ...
+
+
+class MemoryLinearStorage:
+    """
+    A linear storage that stores messages in memory, only for testing.
+    """
+
+    def __init__(self):
+        self._messages: list[Message] = []
+
+    async def append_message(self, message: Message):
+        self._messages.append(message)
+
+
+class JsonlLinearStorage:
+    """
+    A linear storage that stores messages in a JSONL file.
+    """
+
+    def __init__(self, path: Path | str):
+        self._path = path
+        self._file: IO[str] | None = None
+        self._dump_fn = lambda m: m.model_dump(exclude_none=True)
+
+    def _get_file(self) -> IO[str]:
+        if self._file is None:
+            self._file = open(self._path, "w", encoding="utf-8")  # noqa: SIM115
+        return self._file
+
+    def __del__(self):
+        if self._file:
+            self._file.close()
+
+    async def append_message(self, message: Message):
+        file = self._get_file()
+
+        def _write():
+            json.dump(self._dump_fn(message), file, ensure_ascii=False, separators=(",", ":"))
+            file.write("\n")
+
+        await asyncio.to_thread(_write)
--- tests/test_context.py
+++ tests/test_context.py
@@ -1,12 +1,18 @@
 import asyncio
+from pathlib import Path
 
 from kosong.base.message import Message
 from kosong.context import LinearContext
+from kosong.context.linear import JsonlLinearStorage, MemoryLinearStorage
 from kosong.tooling import EmptyToolset
 
 
 def test_linear_context():
-    context = LinearContext(system_prompt="abc", toolset=EmptyToolset())
+    context = LinearContext(
+        system_prompt="abc",
+        toolset=EmptyToolset(),
+        storage=MemoryLinearStorage(),
+    )
     assert context.system_prompt == "abc"
     assert context.history == []
     assert isinstance(context.toolset, EmptyToolset)
@@ -21,3 +27,31 @@ def test_linear_context():
         Message(role="user", content="abc"),
         Message(role="assistant", content="def"),
     ]
+
+
+def test_linear_context_with_jsonl_storage():
+    test_path = Path(__file__).parent / "test.jsonl"
+
+    async def run():
+        storage = JsonlLinearStorage(path=test_path)
+        context = LinearContext(
+            system_prompt="abc",
+            toolset=EmptyToolset(),
+            storage=storage,
+        )
+        await context.add_message(Message(role="user", content="abc"))
+        await context.add_message(Message(role="assistant", content="def"))
+        return context.history
+
+    history = asyncio.run(run())
+    assert history == [
+        Message(role="user", content="abc"),
+        Message(role="assistant", content="def"),
+    ]
+
+    with open(test_path) as f:
+        expected = """\
+{"role":"user","content":"abc"}
+{"role":"assistant","content":"def"}
+"""
+        assert f.read() == expected
