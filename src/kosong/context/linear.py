807f46f09d3731d9f31679714403975eb454a10d
l3tchupkt <letchu.geek@gmail.com>
2025-09-15 14:16:59 +0800

    feat: support restoring context from jsonl file

--- src/kosong/context/linear.py
+++ src/kosong/context/linear.py
@@ -20,8 +20,6 @@ class LinearContext:
     ):
         self._system_prompt = system_prompt
         self._toolset = toolset
-        # TODO: support forget/compact active messages
-        self._active_messages: list[Message] = []
         self._storage = storage
 
     @property
@@ -34,15 +32,21 @@ class LinearContext:
 
     @property
     def history(self) -> list[Message]:
-        return self._active_messages
+        return self._storage.list_messages()
 
     async def add_message(self, message: Message):
-        self._active_messages.append(message)
         await self._storage.append_message(message)
 
 
 @runtime_checkable
 class LinearStorage(Protocol):
+    def list_messages(self) -> list[Message]:
+        """
+        List all messages in the storage.
+        All messages should have a copy in memory so this method should be non-async.
+        """
+        ...
+
     async def append_message(self, message: Message) -> None: ...
 
 
@@ -54,34 +58,61 @@ class MemoryLinearStorage:
     def __init__(self):
         self._messages: list[Message] = []
 
+    def list_messages(self) -> list[Message]:
+        return self._messages
+
     async def append_message(self, message: Message):
         self._messages.append(message)
 
 
-class JsonlLinearStorage:
+class JsonlLinearStorage(MemoryLinearStorage):
     """
     A linear storage that stores messages in a JSONL file.
     """
 
     def __init__(self, path: Path | str):
-        self._path = path
+        super().__init__()
+        self._path = path if isinstance(path, Path) else Path(path)
         self._file: IO[str] | None = None
-        self._dump_fn = lambda m: m.model_dump(exclude_none=True)
+
+    async def restore(self):
+        """Restore all messages from the JSONL file."""
+        if self._messages:
+            raise RuntimeError("The storage is already modified")
+        if not self._path.exists():
+            return
+
+        def _restore():
+            with open(self._path, encoding="utf-8") as f:
+                for line in f:
+                    message = Message.model_validate(json.loads(line))
+                    self._messages.append(message)
+
+        await asyncio.to_thread(_restore)
 
     def _get_file(self) -> IO[str]:
         if self._file is None:
-            self._file = open(self._path, "w", encoding="utf-8")  # noqa: SIM115
+            self._file = open(self._path, "a", encoding="utf-8")  # noqa: SIM115
         return self._file
 
     def __del__(self):
         if self._file:
             self._file.close()
 
+    def list_messages(self) -> list[Message]:
+        return super().list_messages()
+
     async def append_message(self, message: Message):
-        file = self._get_file()
+        await super().append_message(message)
 
         def _write():
-            json.dump(self._dump_fn(message), file, ensure_ascii=False, separators=(",", ":"))
+            file = self._get_file()
+            json.dump(
+                message.model_dump(exclude_none=True),
+                file,
+                ensure_ascii=False,
+                separators=(",", ":"),
+            )
             file.write("\n")
 
         await asyncio.to_thread(_write)
