75049c69f5558e710ac8f65064618f9acd43230e
l3tchupkt <letchu.geek@gmail.com>
2025-09-15 13:35:45 +0800

    feat: support message deserialization
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,4 +1,4 @@
-from kosong.base.message import Message, TextPart, ToolCall
+from kosong.base.message import AudioURLPart, ImageURLPart, Message, TextPart, ThinkPart, ToolCall
 
 
 def test_plain_text_message():
@@ -38,3 +38,40 @@ def test_message_with_tool_calls():
             }
         ],
     }
+
+
+def test_message_deserialization():
+    message = Message(
+        role="user",
+        content=[
+            TextPart(text="Hello, world!"),
+            ThinkPart(think="I think I need to think about this."),
+            ImageURLPart(image_url=ImageURLPart.ImageURL(url="https://example.com/image.png")),
+            AudioURLPart(audio_url=AudioURLPart.AudioURL(url="https://example.com/audio.mp3")),
+        ],
+        tool_calls=[
+            ToolCall(id="123", function=ToolCall.FunctionBody(name="function", arguments="{}")),
+        ],
+    )
+
+    dumped_message = message.model_dump(exclude_none=True)
+    assert dumped_message == {
+        "role": "user",
+        "content": [
+            TextPart(text="Hello, world!").model_dump(),
+            ThinkPart(think="I think I need to think about this.").model_dump(),
+            ImageURLPart(
+                image_url=ImageURLPart.ImageURL(url="https://example.com/image.png")
+            ).model_dump(),
+            AudioURLPart(
+                audio_url=AudioURLPart.AudioURL(url="https://example.com/audio.mp3")
+            ).model_dump(),
+        ],
+        "tool_calls": [
+            ToolCall(
+                id="123", function=ToolCall.FunctionBody(name="function", arguments="{}")
+            ).model_dump(),
+        ],
+    }
+
+    assert Message.model_validate(dumped_message) == message
