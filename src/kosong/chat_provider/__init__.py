3443a4a4d02f620246c43617b31e0e803e5f9906
l3tchupkt <letchu.geek@gmail.com>
2025-09-22 09:41:22 +0800

    feat: add ChaosChatProvider
    Signed-off-by: Richard Chien <stdrc@outlook.com>
    Co-authored-by: xxchan <xxchan22f@gmail.com>
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- src/kosong/chat_provider/__init__.py
+++ src/kosong/chat_provider/__init__.py
@@ -1,21 +1,25 @@
 from kosong.base.chat_provider import ChatProvider
 
 __all__ = [
-    "MockChatProvider",
     "OpenAILegacy",
     "Kimi",
+    # for testing
+    "MockChatProvider",
+    "ChaosChatProvider",
 ]
 
 
 def __static_check_types(
-    mock: "MockChatProvider",
     openai: "OpenAILegacy",
     kimi: "Kimi",
+    mock: "MockChatProvider",
+    chaos: "ChaosChatProvider",
 ):
     """Use type checking to ensure the types are correct implemented."""
     _: ChatProvider = openai
     _: ChatProvider = mock
     _: ChatProvider = kimi
+    _: ChatProvider = chaos
 
 
 class ChatProviderError(Exception):
@@ -25,6 +29,25 @@ class ChatProviderError(Exception):
         super().__init__(message)
 
 
+class APIConnectionError(ChatProviderError):
+    """The error raised when the API connection fails."""
+
+
+class APITimeoutError(ChatProviderError):
+    """The error raised when the API request times out."""
+
+
+class APIStatusError(ChatProviderError):
+    """The error raised when the API returns a status code of 4xx or 5xx."""
+
+    status_code: int
+
+    def __init__(self, status_code: int, message: str):
+        super().__init__(message)
+        self.status_code = status_code
+
+
+from .chaos import ChaosChatProvider  # noqa: E402
 from .kimi import Kimi  # noqa: E402
 from .mock import MockChatProvider  # noqa: E402
 from .openai_legacy import OpenAILegacy  # noqa: E402
--- tests/test_chat_provider.py
+++ tests/test_chat_provider.py
@@ -1,7 +1,11 @@
 import asyncio
 
+import pytest
+
 from kosong.base.chat_provider import StreamedMessagePart
-from kosong.base.message import TextPart
+from kosong.base.message import Message, TextPart
+from kosong.chat_provider import APIStatusError
+from kosong.chat_provider.chaos import ChaosChatProvider, ChaosConfig
 from kosong.chat_provider.mock import MockChatProvider
 
 
@@ -19,3 +23,22 @@ def test_mock_chat_provider():
 
     output_parts = asyncio.run(generate())
     assert output_parts == input_parts
+
+
+@pytest.mark.asyncio
+async def test_chaos_chat_provider():
+    chat_provider = ChaosChatProvider(
+        model="dummy", api_key="sk-1234567890", chaos_config=ChaosConfig(error_probability=1.0)
+    )
+    for _ in range(3):
+        try:
+            parts = []
+            async for part in await chat_provider.generate(
+                system_prompt="",
+                tools=[],
+                history=[Message(role="user", content=[TextPart(text="Hello, world!")])],
+            ):
+                parts.append(part)
+            raise AssertionError("Expected APIStatusError")
+        except APIStatusError:
+            pass
